316b7d41fd6ea5a4f9fbac54e2a4d471cb69405c
Marc Poppy <poppy.marc@gmail.com>
2019-09-29 23:53:35 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE projects (
+CREATE TABLE IF NOT EXISTS projects (
   id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
@@ -7,12 +7,12 @@ CREATE TABLE projects (
   start_date TEXT,
   end_date TEXT
 );
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER,
 );
-CREATE TABLE pledges (
+CREATE TABLE IF NOT EXISTS pledges (
   id INTEGER PRIMARY KEY,
   amount INTEGER,
   user_id INTEGER,
