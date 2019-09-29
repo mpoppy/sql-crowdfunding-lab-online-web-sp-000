f494f6965836bbf0cc467551020ce3e277da6f7b
Marc Poppy <poppy.marc@gmail.com>
2019-09-29 23:53:40 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -6,12 +6,12 @@ CREATE TABLE IF NOT EXISTS projects (
   funding_goal INTEGER,
   start_date TEXT,
   end_date TEXT
-);
+)
 CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER,
-);
+)
 CREATE TABLE IF NOT EXISTS pledges (
   id INTEGER PRIMARY KEY,
   amount INTEGER,
