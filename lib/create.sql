561d3f960685e778f605ea3d301bc946449b5f3c
Marc Poppy <poppy.marc@gmail.com>
2019-09-29 23:50:17 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,13 +1,13 @@
 
--- CREATE TABLE projects (
---   id INTEGER PRIMARY KEY,
---   title TEXT,
---   category TEXT,
---   funding_goal INTEGER,
---   start_date TEXT,
---   end_date TEXT
--- )
---
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
+)
+
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
