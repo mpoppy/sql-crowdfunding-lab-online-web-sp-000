f84ce335721796ba672ca975704ca83b609fd3a6
Marc Poppy <poppy.marc@gmail.com>
2019-09-30 01:15:34 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE IF NOT EXISTS projects(
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT);
+
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER);
+
+CREATE TABLE IF NOT EXISTS pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+);
