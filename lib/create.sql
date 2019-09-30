680930628c531aca59dfbe92713d4eea5cb9c918
Marc Poppy <poppy.marc@gmail.com>
2019-09-30 00:02:32 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -7,15 +7,15 @@ CREATE TABLE IF NOT EXISTS projects(
   start_date TEXT,
   end_date TEXT);
 
-CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  age INTEGER,
-);
-
-CREATE TABLE IF NOT EXISTS pledges (
-  id INTEGER PRIMARY KEY,
-  amount INTEGER,
-  user_id INTEGER,
-  project_id INTEGER
-);
+-- CREATE TABLE IF NOT EXISTS users (
+--   id INTEGER PRIMARY KEY,
+--   name TEXT,
+--   age INTEGER,
+-- );
+--
+-- CREATE TABLE IF NOT EXISTS pledges (
+--   id INTEGER PRIMARY KEY,
+--   amount INTEGER,
+--   user_id INTEGER,
+--   project_id INTEGER
+-- );
