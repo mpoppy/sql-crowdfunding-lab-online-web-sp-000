a39ff087ed76505a4375cbb3b63e1747a5b212e3
Marc Poppy <poppy.marc@gmail.com>
2019-09-30 00:13:01 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,9 +10,10 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   #select the titles of all projects from project TABLE
   #select amount from pledge by joining pledge project_id = project.id
   #order by asc
-  SELECT title, amount
+  SELECT projects.title, pledges.amount
   FROM projects
-  INNER JOIN
+  INNER JOIN pledges
+  ON pledges.project_id = projects.id
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
