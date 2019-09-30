30c3aee5f1d92b78528ddcff224378a81b855e01
Marc Poppy <poppy.marc@gmail.com>
2019-09-30 01:06:53 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,11 +30,11 @@ GROUP BY projects.title HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "SELECT users.name, sum(pledges.amount)
+  "SELECT users.name, pledges.amount
 FROM users
 INNER JOIN pledges on users.id = pledges.id
 GROUP BY users.name
-ORDER BY sum(pledges.amount) ASC;
+ORDER BY pledges.amount;
 "
 end
 
