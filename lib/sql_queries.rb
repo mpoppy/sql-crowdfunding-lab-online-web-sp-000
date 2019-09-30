bbcfb872b6e1924199a1d6cb1669535318a4bb07
Marc Poppy <poppy.marc@gmail.com>
2019-09-30 00:10:34 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,9 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  
+  #select the titles of all projects from project TABLE
+  #select amount from pledge by joining pledge project_id = project.id
+  #order by asc
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
