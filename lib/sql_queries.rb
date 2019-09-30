4f20f19ac35c01efad3b72f3d097b80ca4048e81
Marc Poppy <poppy.marc@gmail.com>
2019-09-30 00:16:32 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,8 +15,6 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   # INNER JOIN pledges
   # ON pledges.project_id = projects.id
   # ORDER BY asc;
-
-  select title FROM projects;
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
